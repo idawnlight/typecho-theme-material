50d885934b856ecd0c5dd82bf6ca62b9e37d7313
黎明余光 <i@lim-light.com>
2017-07-13 19:53:43 +0800

    Add Check Update
    优化想法

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php
 
+define(MATERIAL_VERSION,"3.0.0");
+
 //Appearance setup
 function themeConfig($form)
 {
@@ -14,7 +16,7 @@ function themeConfig($form)
     <a href="https://github.com/LiMingYuGuang/typecho-theme-material/issues" target="_blank">建议&反馈</a>
     </p>';
 
-    echo '<span id="update">正在检查主题更新</span><script src="https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js"></script><script>$(document).ready(function(){$(".loading").hide();getupdate()});function getupdate(){$.ajax({url:"https://api.lim-light.com/update/material.php?version=3.0.0",dataType:"jsonp",async:true,jsonp:"callback",jsonpCallback:"checkupdate",success:function(result){$("#update").html(result.msg)},error:function(){$("#update").html("获取更新失败，API服务器可能挂了...")}})};</script>';
+    echo '<span id="update">正在检查主题更新</span><script src="https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js"></script><script>$(document).ready(function(){$(".loading").hide();getupdate()});function getupdate(){$.ajax({url:"https://api.lim-light.com/update/material.php?version='.MATERIAL_VERSION.',dataType:"jsonp",async:true,jsonp:"callback",jsonpCallback:"checkupdate",success:function(result){$("#update").html(result.msg)},error:function(){$("#update").html("获取更新失败，API服务器可能挂了...")}})};</script>';
 
     $switch = new Typecho_Widget_Helper_Form_Element_Checkbox('switch',
         array(
