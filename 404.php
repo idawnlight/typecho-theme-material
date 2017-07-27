f6146ebee78d567ad34febf91d12eb7e1a43d564
黎明余光 <m1255162259@gmail.com>
2017-07-27 17:18:59 +0800

    [Fix] 404页
    - Typecho 的 404 页，全是坑！

--- 404.php
+++ 404.php
@@ -4,10 +4,6 @@
 
     <main class="material-layout__content" id="main">
         <div id="top"></div>
-        <!-- Sidebar hamburger button -->
-        <button class="MD-burger-icon sidebar-toggle">
-            <span class="MD-burger-layer"></span>
-        </button>
 
         <!-- Post module -->
         <div class="material-post_container">
@@ -21,30 +17,100 @@
                         </p>
                     </div>
 
-                <!-- Articel content -->
-                <div id="post-content" class="mdl-color-text--grey-700 mdl-card__supporting-text fade out">
-                    <br>
-                    <h1>404 - Not Found</h1>
-                    <h3><?php echo tranMsg("The requested resource is not available", "您所请求的资源不可用", $this->options->langis) ?></h3>
-                    <br><br>
-                </div>
+                    <!-- Articel content -->
+                    <div id="post-content" class="mdl-color-text--grey-700 mdl-card__supporting-text fade out">
+                        <br>
+                        <h1>404 - Not Found</h1>
+                        <h3>The requested resource is not available</h3>
+                        <br><br>
+                    </div>
 
-                <!-- Search -->
-                <div class="mdl-color-text--primary-contrast mdl-card__supporting-text comments">
-                    <form method="post" action="">
-                        <!-- Input search content -->
-                        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label" id="comment-input-div">
-                            <textarea name="text" rows="1" id="comment" class="mdl-textfield__input"></textarea>
-                            <label for="comment" class="mdl-textfield__label">
-                                <?php echo tranMsg("Search Engine may help you find out new pages", "试试搜索...", $this->options->langis) ?>
-                            </label>
-                        </div>
-                    </form>
-                </div>
+                    <!-- Search -->
+                    <div class="mdl-color-text--primary-contrast mdl-card__supporting-text comments">
+                        <form method="post" action="">
+                            <!-- Input search content -->
+                            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label" id="comment-input-div">
+                                    <input class="mdl-textfield__input" type="text" name="s">
+                                    <label class="mdl-textfield__label" for="sample3">Search Engine may help you find out new pages</label>
 
-            </div>
+                            </div>
+                        </form>
+                    </div>
+
+                </div>
 
         </div>
 
-        <?php include('sidebar.php'); ?>
-        <?php include('footer.php'); ?>
+    </main>
+    <div class="mdl-layout__obfuscator"></div>
+</div>
+
+
+<!--Analysis code-->
+<?php $this->options->analysis(); ?>
+</body>
+
+<!-- Material js -->
+<?php if (!empty($this->options->CDNURL)): ?>
+<script src="<?php $this->options->CDNURL() ?>/MaterialCDN/js/jquery.min.js"></script>
+<script src="<?php $this->options->CDNURL() ?>/MaterialCDN/js/js.min.js"></script>
+<script src="<?php $this->options->CDNURL() ?>/MaterialCDN/js/jquery.pjax.min.js"></script>
+<?php else: ?>
+<script src="<?php $this->options->themeUrl('js/jquery.min.js'); ?>"></script>
+<script src="<?php $this->options->themeUrl('js/js.min.js'); ?>"></script>
+<script src="<?php $this->options->themeUrl('js/jquery.pjax.min.js'); ?>"></script>
+<?php endif; ?>
+
+
+<?php if (!empty($this->options->switch) && in_array('ShowLoadingLine', $this->options->switch)): ?>
+<?php if (!empty($this->options->CDNURL)): ?>
+<script src="<?php $this->options->CDNURL() ?>/MaterialCDN/js/nprogress.js"></script>
+<?php else: ?>
+<script src="<?php $this->options->themeUrl('js/nprogress.js'); ?>"></script>
+<?php endif; ?>
+
+<script type="text/javascript">
+    NProgress.configure({
+        showSpinner: true
+    });
+    NProgress.start();
+    $('#nprogress .bar').css({
+        'background': '<?php $this->options->loadingcolor(); ?>'
+    });
+    $('#nprogress .peg').css({
+        'box-shadow': '0 0 10px <?php $this->options->loadingcolor(); ?>, 0 0 15px <?php $this->options->loadingcolor(); ?>'
+    });
+    $('#nprogress .spinner-icon').css({
+        'border-top-color': '<?php $this->options->loadingcolor(); ?>',
+        'border-left-color': '<?php $this->options->loadingcolor(); ?>'
+    });
+    setTimeout(function() {
+        NProgress.done();
+        $('.fade').removeClass('out');
+    }, <?php $this->options->loadingbuffer(); ?>);
+</script>
+<?php endif; ?>
+
+<?php if (!empty($this->options->switch) && in_array('SmoothScroll', $this->options->switch)): ?>
+<?php if (!empty($this->options->CDNURL)): ?>
+<script src="<?php $this->options->CDNURL() ?>/MaterialCDN/js/smoothscroll.js" async></script>
+<?php else: ?>
+<script src="<?php $this->options->themeUrl('js/smoothscroll.js'); ?>" async></script>
+<?php endif; ?>
+<?php endif; ?>
+
+
+<!-- Pangu js -->
+<?php if (!empty($this->options->switch) && in_array('Pangu', $this->options->switch)): ?>
+  <?php if (!empty($this->options->CDNURL)): ?>
+      <script src="<?php $this->options->CDNURL() ?>/MaterialCDN/js/pangu.min.js"></script>
+  <?php else: ?>
+      <script src="<?php $this->options->themeUrl('js/pangu.min.js'); ?>"></script>
+  <?php endif; ?>
+  <script> pangu.spacingPage(); </script>
+<?php endif; ?>
+
+
+<?php $this->footer(); ?>
+
+</html>
