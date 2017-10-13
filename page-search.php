269ccd0df8b4c0bc9d0f09cc6d8565a0315f74d7
黎明余光 <m1255162259@gmail.com>
2017-10-13 23:03:43 +0800

    [Imp] Better localsearch

--- page-search.php
+++ page-search.php
@@ -6,19 +6,19 @@
  */
 
 header("content-type:text/xml");
+
 $this->widget('Widget_Contents_Post_Recent', 'pageSize=10000')->to($archive);
+
 echo "<search>\n";
-foreach ($archive->stack as $info) {
-    echo "<entry>\n";
-    echo "<title>\n<![CDATA[{$info["title"]}]]>\n</title>\n";
-    echo "<url>\n{$info["permalink"]}\n</url>\n";
-    echo "<content type=\"html\">\n<![CDATA[{$info["text"]}]]>\n</content>\n";
-    echo "<categories>\n";
-    foreach ($info["categories"] as $categories) {
-        echo "\n<category>\n{$categories["name"]}\n</category>";
-    }
-    echo "\n</categories>\n";
-    echo "<tags></tags>";
-    echo "</entry>\n";
-}
+while ($archive->next()):
+    echo "\t<entry>\n";
+    echo "\t\t<title>\n\t\t\t<![CDATA["; $archive->title(); echo "]]>\n\t\t</title>\n";
+    echo "\t\t<url>\n\t\t\t"; $archive->permalink(); echo "\n\t\t</url>\n";
+    echo "\t\t<content type=\"html\">\n\t\t<![CDATA["; $archive->content(); echo "]]>\n\t\t</content>\n";
+    echo "\t\t<categories>\n\t\t\t";
+    $archive->category(',', false);
+    echo "\n\t\t</categories>\n";
+    echo "\t\t<tags>\n\t\t\t"; $archive->tags(',', false); echo "\n\t\t</tags>\n";
+    echo "\t</entry>\n";
+endwhile;
 echo "</search>";
